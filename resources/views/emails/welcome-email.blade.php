58636feeadb233b6150f2a26eb1af0f4dbd61d78
JStefan <jitianu_stefan@yahoo.com>
2022-10-07 21:32:30 +0300

    Really the last commit/ End of the project

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome to my first Laravel Project!!!
+
+This is a community a fellow developers and we love that you have joined us.
+
+All the best,<br>
+Stefan
+<!-- {{ config('app.name') }} -->
+@endcomponent
